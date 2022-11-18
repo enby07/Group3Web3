9a81c617584016d8880e2f91bd177120b112be9a
enby07 <en.by.07@gmail.com>
2022-11-18 18:57:47 +0100

    Add project files.

---
+++ Group3Web3/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Group3Web3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group3Web3.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Raporti()
+        {
+            Studenti st1 = new Studenti("Enis", "Bajrami");
+            Studenti st2 = new Studenti("Enis2", "Bajrami2");
+            Studenti st3 = new Studenti("Enis3", "Bajrami3");
+
+            List<Studenti> studentet = new List<Studenti>();
+            studentet.Add(st1);
+            studentet.Add(st2);
+            studentet.Add(st3);
+
+            return View(studentet);
+        }
+
+        public IActionResult MesoHtml()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ Group3Web3/Models/Studenti.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group3Web3.Models
+{
+    public class Studenti
+    {
+        public Studenti(string emri, string mbiemri)
+        {
+            Emri = emri;
+            Mbiemri = mbiemri;
+
+        }
+        public string Emri { get; set; }
+        
+        public string Mbiemri { get; set; }
+    }
+}
